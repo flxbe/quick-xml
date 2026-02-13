f45eec8ce8d6fa1f14e27a11f455aec6a134c575
Felix Bernhardt <felix.bernhardt@mailbox.org>
2026-02-13 13:28:35 +0100

    fix benchmark

--- benches/flxbe.rs
+++ benches/flxbe.rs
@@ -4,34 +4,31 @@
 #![allow(clippy::incompatible_msrv)]
 use criterion::{self, criterion_group, criterion_main, Criterion, Throughput};
 use quick_xml::events::Event;
-use quick_xml::reader::NsReader;
-use quick_xml::Result as XmlResult;
+use quick_xml::{Reader, Result as XmlResult};
 use std::hint::black_box;
 
 static BOB: &str = include_str!("../tests/documents/bob.xml");
 
 // TODO: use fully normalized attribute values
 fn parse_document_from_bytes_with_namespaces(input: &str) -> XmlResult<()> {
-    let mut r = NsReader::from_str(input);
+    let mut r = Reader::from_str(input);
     let mut buf = Vec::new();
     loop {
-        match black_box(r.read_resolved_event_into(&mut buf)?) {
-            (resolved_ns, Event::Start(e) | Event::Empty(e)) => {
-                black_box(resolved_ns);
+        match black_box(r.read_event_into(&mut buf)?) {
+            Event::Start(e) | Event::Empty(e) => {
+                black_box(e.local_name());
                 for attr in e.attributes() {
                     black_box(attr?.decode_and_unescape_value(r.decoder())?);
                 }
             }
-            (resolved_ns, Event::Text(e)) => {
+            Event::Text(e) => {
                 black_box(e.xml_content()?);
-                black_box(resolved_ns);
             }
-            (resolved_ns, Event::CData(e)) => {
+            Event::CData(e) => {
                 black_box(e.into_inner());
-                black_box(resolved_ns);
             }
-            (_, Event::End(_)) => (),
-            (_, Event::Eof) => break,
+            Event::End(_) => (),
+            Event::Eof => break,
             _ => (),
         }
         // buf.clear();
