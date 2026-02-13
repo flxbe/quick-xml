7feb5ad80e36982e653ca1c4766de5294505238a
Felix Bernhardt <felix.bernhardt@mailbox.org>
2026-02-13 14:09:33 +0100

    extend benchmark

--- benches/flxbe.rs
+++ benches/flxbe.rs
@@ -9,8 +9,32 @@ use std::hint::black_box;
 
 static BOB: &str = include_str!("../tests/documents/bob.xml");
 
-// TODO: use fully normalized attribute values
-fn parse_document_from_bytes_with_namespaces(input: &str) -> XmlResult<()> {
+fn parse_from_reader(input: &str) -> XmlResult<()> {
+    let mut r = Reader::from_str(input);
+    loop {
+        match black_box(r.read_event()?) {
+            Event::Start(e) | Event::Empty(e) => {
+                black_box(e.local_name());
+                for attr in e.attributes() {
+                    black_box(attr?.decode_and_unescape_value(r.decoder())?);
+                }
+            }
+            Event::Text(e) => {
+                black_box(e.xml_content()?);
+            }
+            Event::CData(e) => {
+                black_box(e.into_inner());
+            }
+            Event::End(_) => (),
+            Event::Eof => break,
+            _ => (),
+        }
+        // buf.clear();
+    }
+    Ok(())
+}
+
+fn parse_from_slice(input: &str) -> XmlResult<()> {
     let mut r = Reader::from_str(input);
     let mut buf = Vec::new();
     loop {
@@ -36,18 +60,25 @@ fn parse_document_from_bytes_with_namespaces(input: &str) -> XmlResult<()> {
     Ok(())
 }
 
-/// Decode into a buffer, then parse - including namespaces
-pub fn bench_decode_and_parse_document_with_namespaces(c: &mut Criterion) {
-    let mut group = c.benchmark_group("parse_bob_from_file");
+pub fn bench_parse_from_reader(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse_from_reader");
 
     group.throughput(Throughput::Bytes(BOB.len() as u64));
 
-    group.bench_with_input("BOB", BOB, |b, input| {
-        b.iter(|| parse_document_from_bytes_with_namespaces(input))
-    });
+    group.bench_with_input("BOB", BOB, |b, input| b.iter(|| parse_from_reader(input)));
 
     group.finish();
 }
 
-criterion_group!(benches, bench_decode_and_parse_document_with_namespaces,);
+pub fn bench_parse_from_slice(c: &mut Criterion) {
+    let mut group = c.benchmark_group("parse_from_slice");
+
+    group.throughput(Throughput::Bytes(BOB.len() as u64));
+
+    group.bench_with_input("BOB", BOB, |b, input| b.iter(|| parse_from_slice(input)));
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_parse_from_slice, bench_parse_from_reader);
 criterion_main!(benches);
